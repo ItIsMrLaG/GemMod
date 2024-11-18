18a451088e6d5a0ced52ae39005fc7952c8e78df
juliakononov <julickk04@mail.ru>
2024-11-18 19:55:52 +0300

    feat(grid): all status update function

--- cat_simulation/grid.py
+++ cat_simulation/grid.py
@@ -74,3 +74,66 @@ def setup_grid(
     _F_COLUMN_SUM = ti.field(dtype=ti.i32, shape=(_GRID_COL_N,))
     _F_PREFIX_SUM = ti.field(dtype=ti.i32, shape=(_GRID_COL_N, _GRID_ROW_N))
     _F_CELL_CUR = ti.field(dtype=ti.i32, shape=(_CELL_N,))
+
+
+@ti.func
+def init_cell_storage(cats: ti.template()):
+    _F_CAT_PER_CELL.fill(0)
+
+    for idx in range(_CATS_N):
+        cell_idx = ti.floor(cats[idx].point / _CELL_SZ, ti.i32)
+        ti.atomic_add(_F_CAT_PER_CELL[cell_idx], 1)
+
+    for col in range(_GRID_COL_N):
+        _sum = 0
+        for row in range(_GRID_ROW_N):
+            _sum += _F_CAT_PER_CELL[col, row]
+        _F_COLUMN_SUM[col] = _sum
+
+    # Compute NOT in parallel
+    _F_PREFIX_SUM[0, 0] = 0
+    ti.loop_config(serialize=True)
+    for col in range(1, _GRID_COL_N):
+        _F_PREFIX_SUM[col, 0] = _F_PREFIX_SUM[col - 1, 0] + _F_COLUMN_SUM[col - 1]
+
+    for col in range(_GRID_COL_N):
+        for row in range(_GRID_ROW_N):
+            if row == 0:
+                _F_PREFIX_SUM[col, row] += _F_CAT_PER_CELL[col, row]
+            else:
+                _F_PREFIX_SUM[col, row] = _F_PREFIX_SUM[col, row - 1] + _F_CAT_PER_CELL[col, row]
+
+            cell_lin_idx = col * _GRID_ROW_N + row
+            head = _F_PREFIX_SUM[col, row] - _F_CAT_PER_CELL[col, row]
+            _F_CELL_HEADS[cell_lin_idx] = head
+            _F_CELL_CUR[cell_lin_idx] = head
+    _F_CELL_HEADS[_CELL_N] = _F_PREFIX_SUM[_GRID_COL_N - 1, _GRID_ROW_N - 1]
+
+    for idx in range(_CATS_N):
+        cell_idx = ti.floor(cats[idx].point / _CELL_SZ, ti.i32)
+        cell_lin_idx = cell_idx[0] * _GRID_ROW_N + cell_idx[1]
+        cat_cell_location = ti.atomic_add(_F_CELL_CUR[cell_lin_idx], 1)
+        _F_CELL_STORAGE[cat_cell_location] = idx
+
+
+@ti.kernel
+def update_statuses(cats: ti.template(), distance_type: ti.i32):
+    init_cell_storage(cats)
+
+    for idx1 in range(_CATS_N):
+        cell_idx = ti.floor(cats[idx1].point / _CELL_SZ, ti.i32)
+
+        x_begin = max(cell_idx[0] - 1, 0)
+        x_end = min(cell_idx[0] + 2, _GRID_COL_N)
+        y_begin = max(cell_idx[1] - 1, 0)
+        y_end = min(cell_idx[1] + 2, _GRID_ROW_N)
+
+        for cell_col in range(x_begin, x_end):
+            for cell_row in range(y_begin, y_end):
+                neighbour_lin_idx = cell_col * _GRID_ROW_N + cell_row
+                for _idx2 in range(_F_CELL_HEADS[neighbour_lin_idx],
+                                   _F_CELL_HEADS[neighbour_lin_idx + 1]):
+                    idx2 = _F_CELL_STORAGE[_idx2]
+
+                    if idx1 != idx2:
+                        cats[idx1].fight_with(cats[idx2], distance_type)
