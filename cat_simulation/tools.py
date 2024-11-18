bc839dbbbf53064a6c920d112ef0944a26239b6a
juliakononov <julickk04@mail.ru>
2024-11-18 19:55:02 +0300

    feat(tools): distance functions

--- cat_simulation/tools.py
+++ cat_simulation/tools.py
@@ -1,3 +1,38 @@
+import taichi as ti
+import taichi.math as tm
+
+from cat_simulation.constants import *
+
+
+@ti.func
+def euclidean_distance(point1: tm.vec2, point2: tm.vec2) -> ti.f32:
+    return (point1 - point2).norm()
+
+
+@ti.func
+def manhattan_distance(point1: tm.vec2, point2: tm.vec2) -> ti.f32:
+    return ti.abs(point1.x - point2.x) + ti.abs(point1.y - point2.y)
+
+
+@ti.func
+def chebyshev_distance(point1: tm.vec2, point2: tm.vec2) -> ti.f32:
+    return ti.max(ti.abs(point1.x - point2.x), ti.abs(point1.y - point2.y))
+
+
+@ti.func
+def get_distance(p1: tm.vec2, p2: tm.vec2, distance_type: ti.i32) -> ti.f32:
+    ans: ti.f32 = 0
+
+    if distance_type == MANHATTAN_DISTANCE:
+        ans = manhattan_distance(p1, p2)
+    elif distance_type == CHEBYSHEV_DISTANCE:
+        ans = chebyshev_distance(p1, p2)
+    else:
+        ans = euclidean_distance(p1, p2)
+
+    return ans
+
+
 @ti.func
 def move_pattern_random(point_: tm.vec2) -> tm.vec2:
     # unsigned values
