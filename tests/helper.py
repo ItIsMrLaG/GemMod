8455e6da72b0ec9335aa2bbc0861ca339fc4abe0
juliakononov <julickk04@mail.ru>
2024-11-19 04:42:45 +0300

    feat: grid tests

---
+++ tests/helper.py
@@ -0,0 +1,55 @@
+from cat_simulation.cat import init_cat_env, Cat
+from cat_simulation.constants import *
+import taichi as ti
+from cat_simulation.tools import get_distance
+
+
+def init_cats(n, r0, r1, width, height):
+    init_cat_env(r0, r1, width, height)
+    cats = Cat.field(shape=(n,))
+    return cats
+
+
+def init_consts(n, radius, r0, r1, width, height):
+    cats = init_cats(n, r0, r1, width, height)
+    points = ti.Vector.field(n=2, dtype=float, shape=(n,))
+    return cats, points, radius, n
+
+
+@ti.kernel
+def init_cats_with_custom_points(n: ti.i32, cats: ti.template(), points: ti.template(), radius: ti.f32):
+    for i in range(n):
+        cats[i].radius = radius
+        cats[i]._set_point(points[i])
+        cats[i].move_pattern = MOVE_PATTERN_RANDOM_ID
+        cats[i].status = INTERACTION_NO
+        cats[i].color = GREEN_COLOR
+
+
+@ti.kernel
+def set_cat_init_positions(n: ti.i32, r: ti.f32, cats: ti.template()):
+    for idx in range(n):
+        cats[idx].init_cat(r)
+
+
+@ti.kernel
+def primitive_update_states(
+        n: ti.i32,
+        cats: ti.template(),
+        statuses: ti.types.ndarray(),
+        distance_type: ti.i32,
+        r0: ti.f32,
+        r1: ti.f32
+):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            dist = get_distance(cats[i].point, cats[j].point, distance_type)
+            if dist <= r0:
+                statuses[i] = INTERACTION_LEVEL_0
+            elif dist <= r1:
+                statuses[i] = ti.max(INTERACTION_LEVEL_1, statuses[i])
+            else:
+                statuses[i] = ti.max(INTERACTION_NO, statuses[i])
+
---
+++ tests/test_grid.py
@@ -0,0 +1,61 @@
+import taichi as ti
+import taichi.math as tm
+from cat_simulation.constants import *
+from cat_simulation.grid import setup_grid, update_statuses
+from tests.helper import init_consts, init_cats_with_custom_points, init_cats, set_cat_init_positions, primitive_update_states
+import pytest
+
+
+@ti.data_oriented
+class TestUpdateStatus:
+    def test_concrete_case(self):
+        F_CATS, F_POINTS, RADIUS, N = init_consts(5, 1, 2, 8, 50, 50)
+        F_POINTS[0] = tm.vec2(0.0, 0.0)
+        F_POINTS[1] = tm.vec2(2.0, 0.0)
+        F_POINTS[2] = tm.vec2(8.0, 0.0)
+        F_POINTS[3] = tm.vec2(10.0, 10.0)
+        F_POINTS[4] = tm.vec2(50.0, 50.0)
+        init_cats_with_custom_points(N, F_CATS, F_POINTS, RADIUS)
+        setup_grid(N, 8.0, 50.0, 50.0)
+        update_statuses(F_CATS, EUCLIDEAN_DISTANCE)
+
+        expected_statuses = [
+            INTERACTION_LEVEL_0,
+            INTERACTION_LEVEL_0,
+            INTERACTION_LEVEL_1,
+            INTERACTION_NO,
+            INTERACTION_NO,
+        ]
+        expected_colors = [
+            RED_COLOR,
+            RED_COLOR,
+            YELLOW_COLOR,
+            GREEN_COLOR,
+            GREEN_COLOR,
+        ]
+
+        for i in range(N):
+            assert F_CATS[i].status == expected_statuses[i]
+            assert F_CATS[i].color == expected_colors[i]
+
+    @pytest.mark.parametrize(
+        "N, R0, R1, RADIUS, WIDTH, HEIGHT, distance_type",
+        [
+            (10, 2, 8, 1, 100, 100, EUCLIDEAN_DISTANCE),
+            (100, 2, 8, 1, 500, 500, EUCLIDEAN_DISTANCE),
+            (1000, 2, 8, 1, 500, 500, EUCLIDEAN_DISTANCE),
+            (10000, 2, 8, 1, 1000, 1000, EUCLIDEAN_DISTANCE),
+            (10000, 2, 8, 1, 1500, 2000, EUCLIDEAN_DISTANCE),
+            (50000, 2, 8, 1, 1000, 1000, EUCLIDEAN_DISTANCE),
+        ]
+    )
+    def test_naive_func(self, N, R0, R1, RADIUS, WIDTH, HEIGHT, distance_type):
+        setup_grid(N, R1, WIDTH, HEIGHT)
+        F_CATS = init_cats(N, R0, R1, WIDTH, HEIGHT)
+        set_cat_init_positions(N, RADIUS, F_CATS)
+
+        expected_statuses = ti.ndarray(dtype=ti.i32, shape=(N,))
+        primitive_update_states(N, F_CATS, expected_statuses, distance_type, R0, R1)
+        update_statuses(F_CATS, distance_type)
+        for i in range(N):
+            assert F_CATS[i].status == expected_statuses[i]
