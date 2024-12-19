bb42504eb5aafcdeb8280b2226f4eae1af9745d4
juliakononov <julickk04@mail.ru>
2024-12-19 16:23:33 +0300

    feat: add logs for favorite cat

--- src/catsim/grid.py
+++ src/catsim/grid.py
@@ -1,5 +1,6 @@
 import math
 from typing import Any
+from catsim.enums import INTERACTION_NO, INTERACTION_LEVEL_0, INTERACTION_LEVEL_1
 
 import taichi as ti
 
@@ -18,6 +19,11 @@ _CELL_N: ti.i32
 _CELL_SZ: ti.i32
 _GRID_COL_N: ti.i32
 _GRID_ROW_N: ti.i32
+
+_FAV_CATS_AMOUNT: ti.i32
+_NEW_LOGS: ti.field
+_OLD_LOGS: ti.field
+_OLD_STATUS: ti.field
 
 """
 contains Cats ids:
@@ -46,12 +52,21 @@ _F_PREFIX_SUM: Any
 _F_CAT_PER_CELL: Any
 
 
-def setup_grid(cat_n: ti.i32, r1: ti.i32, width: ti.i32, height: ti.i32):
+def setup_grid(
+    cat_n: ti.i32, r1: ti.i32, width: ti.i32, height: ti.i32, fav_cats_amount: ti.i32
+):
     global _CATS_N, _RADIUS_1, _PLATE_WIDTH, _PLATE_HEIGHT
     _CATS_N = cat_n
     _RADIUS_1 = r1
     _PLATE_WIDTH = width
     _PLATE_HEIGHT = height
+
+    global _FAV_CATS_AMOUNT, _NEW_LOGS, _OLD_LOGS, _OLD_STATUS
+    _FAV_CATS_AMOUNT = fav_cats_amount
+    _NEW_LOGS = ti.field(ti.i32, shape=(fav_cats_amount, _CATS_N))
+    _OLD_LOGS = ti.field(ti.i32, shape=(fav_cats_amount, _CATS_N))
+    _OLD_STATUS = ti.field(ti.i8, shape=(fav_cats_amount,))
+    _OLD_STATUS.fill(1)
 
     global _CELL_N, _GRID_COL_N, _GRID_ROW_N, _CELL_SZ
     _CELL_SZ = _RADIUS_1
@@ -117,6 +132,9 @@ def update_statuses(cats: ti.template()):
     init_cell_storage(cats)
 
     ti.loop_config(serialize=True)
+
+    _NEW_LOGS.fill(0)
+
     for idx1 in range(_CATS_N):
         cell_idx = ti.floor(cats[idx1].point / _CELL_SZ, ti.i32)
 
@@ -135,4 +153,29 @@ def update_statuses(cats: ti.template()):
                     idx2 = _F_CELL_STORAGE[_idx2]
 
                     if idx1 != idx2:
-                        cats[idx1].fight_with(cats[idx2])
+                        status = cats[idx1].fight_with(cats[idx2])
+
+                        if idx1 < _FAV_CATS_AMOUNT:
+                            _NEW_LOGS[idx1, idx2] = status
+
+        for i in range(_FAV_CATS_AMOUNT):
+            has_interaction = False
+            for j in range(_CATS_N):
+                new_val = _NEW_LOGS[i, j]
+                old_val = _OLD_LOGS[i, j]
+                if new_val != INTERACTION_NO:
+                    has_interaction = True
+                    if new_val != old_val:
+                        if new_val == INTERACTION_LEVEL_0:
+                            print(f"CAT {j} IS FIGHTING WITH YOUR CAT {i}")
+                        else:
+                            print(f"CAT {j} HISSED AT YOUR CAT {i}")
+
+                _OLD_LOGS[i, j] = new_val
+
+            if not has_interaction:
+                if _OLD_STATUS[i] == 1:
+                    print(f"YOUR CAT NUMBER {i} IS NOT HURT BY ANYONE")
+                _OLD_STATUS[i] = 0
+            else:
+                _OLD_STATUS[i] = 1
